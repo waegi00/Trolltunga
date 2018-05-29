ef9bf368264a114f07a83fcf7a30ec2eab92426c
waegi <lukas00@bluewin.ch>
2018-05-29 19:58:56 +0200

    Added Developer ViewModel and changed it in controller for edit and create General view fixes

---
+++ Trolltunga/ViewModels/Developer/DeveloperViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Trolltunga.Models;
+
+namespace Trolltunga.ViewModels.Developer
+{
+    public class DeveloperViewModel : Models.Developer
+    {
+        public List<Project> AllProjects { get; set; } = new List<Project>();
+    }
+}
