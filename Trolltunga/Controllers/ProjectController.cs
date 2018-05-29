30c348747b1126d6298110fd9444247da4806b73
waegi <lukas00@bluewin.ch>
2018-05-29 22:20:31 +0200

    authorized project controller and generalized views

---
+++ Trolltunga/Controllers/ProjectController.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web.Mvc;
+using Trolltunga.Models;
+
+namespace Trolltunga.Controllers
+{
+    [Authorize]
+    public class ProjectController : Controller
+    {
+        private readonly ApplicationDbContext _db = new ApplicationDbContext();
+        
+        public ActionResult Index()
+        {
+            return View(_db.Projects.ToList());
+        }
+        
+        public ActionResult Details(Guid? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            var project = _db.Projects.Find(id);
+            if (project == null)
+            {
+                return HttpNotFound();
+            }
+            return View(project);
+        }
+        
+        public ActionResult Create()
+        {
+            return View();
+        }
+        
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Name,Description")] Project project)
+        {
+            if (!ModelState.IsValid) return View(project);
+            project.Id = Guid.NewGuid();
+            _db.Projects.Add(project);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+
+        }
+        
+        public ActionResult Edit(Guid? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            var project = _db.Projects.Find(id);
+            if (project == null)
+            {
+                return HttpNotFound();
+            }
+            return View(project);
+        }
+        
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Name,Description")] Project project)
+        {
+            if (!ModelState.IsValid) return View(project);
+            _db.Entry(project).State = EntityState.Modified;
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        
+        public ActionResult Delete(Guid? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            var project = _db.Projects.Find(id);
+            if (project == null)
+            {
+                return HttpNotFound();
+            }
+            return View(project);
+        }
+        
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(Guid id)
+        {
+            var project = _db.Projects.Find(id);
+            if (project == null) return View("Error");
+            _db.Projects.Remove(project);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                _db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
