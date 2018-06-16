993af04c8ace6bee4a1b9e5af0e7963e25956f14
waegi <lukas00@bluewin.ch>
2018-06-16 22:49:59 +0200

    added task controller including viewmodel edited views and added actions to project dashboard

---
+++ Trolltunga/Controllers/TaskController.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Trolltunga.Models;
+using Trolltunga.ViewModels.Task;
+
+namespace Trolltunga.Controllers
+{
+    public class TaskController : Controller
+    {
+        private readonly ApplicationDbContext _db = new ApplicationDbContext();
+
+        public ActionResult Index(Guid projectId)
+        {
+            return RedirectToAction("Dashboard", "Project", new { id = projectId });
+        }
+
+        public ActionResult Create(Guid projectId)
+        {
+            return View(new TaskFormViewModel
+            {
+                AllUsers = _db.Projects.Find(projectId)?.Participants.ToList(),
+                ProjectId = projectId
+            });
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(TaskFormViewModel model)
+        {
+            if (!ModelState.IsValid) return View(model);
+            var task = new Task
+            {
+                Id = new Guid(),
+                Name = model.Name,
+                Description = model.Description,
+                Status = Status.Todo,
+                ProjectId = model.ProjectId,
+                Assignees = _db.Users.Where(x => model.Participants.Contains(x.Id)).ToList()
+            };
+            _db.Tasks.Add(task);
+            _db.SaveChanges();
+            return RedirectToAction("Index", new { projectId = task.ProjectId });
+        }
+
+        public ActionResult Edit(Guid? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            var task = _db.Tasks.Find(id);
+            if (task == null)
+            {
+                return HttpNotFound();
+            }
+            return View(new TaskFormViewModel
+            {
+                Id = task.Id,
+                Name = task.Name,
+                Description = task.Description,
+                AllUsers = _db.Projects.Find(task.Project.Id)?.Participants.ToList(),
+                ProjectId = task.ProjectId
+            });
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(TaskFormViewModel model)
+        {
+            if (!ModelState.IsValid) return View(model);
+            var task = _db.Tasks.FirstOrDefault(x => x.Id == model.Id);
+            if (task == null) return View("Error");
+            task.Id = model.Id;
+            task.Name = model.Name;
+            task.Description = model.Description;
+            task.Assignees.Clear();
+            task.Assignees = _db.Users.Where(x => model.Participants.Contains(x.Id)).ToList();
+            task.ProjectId = model.ProjectId;
+            _db.Entry(task).State = EntityState.Modified;
+            _db.SaveChanges();
+            return RedirectToAction("Index", new { projectId = task.ProjectId });
+        }
+
+        public ActionResult Delete(Guid? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            var task = _db.Tasks.Find(id);
+            if (task == null)
+            {
+                return HttpNotFound();
+            }
+            return View(new TaskFormViewModel
+            {
+                Id = task.Id,
+                Name = task.Name,
+                Description = task.Description,
+                AllUsers = _db.Projects.Find(task.Project.Id)?.Participants.ToList(),
+                ProjectId = task.ProjectId
+            });
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(Guid id)
+        {
+            var task = _db.Tasks.Find(id);
+            if (task == null) return View("Error");
+            _db.Tasks.Remove(task);
+            _db.SaveChanges();
+            return RedirectToAction("Index", new { projectId = task.ProjectId });
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                _db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Trolltunga/ViewModels/Task/TaskFormViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+using Trolltunga.Models;
+
+
+namespace Trolltunga.ViewModels.Task
+{
+    public class TaskFormViewModel
+    {
+        public Guid Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public Guid ProjectId { get; set; }
+
+        public ICollection<string> Participants { get; set; } = new List<string>();
+
+        public ICollection<ApplicationUser> AllUsers { get; set; } = new List<ApplicationUser>();
+    }
+}
